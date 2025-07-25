797222d3c7d1bb5ee10ac2332681a8a8cc59b042
Juan Torres <jmmtorres1991@yahoo.com>
2025-07-24 23:42:49 -0500

    finished the player class, added groups to containers

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from constants import *
 import pygame
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    run = True
+    clock = pygame.time.Clock()
+    #dt is delta
+    dt = 0
     
-    while run:
+    updateable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+
+    Player.containers = (updateable_group,drawable_group)
+    player1 = Player(SCREEN_WIDTH/ 2,SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return  
+                return     
+             
+        updateable_group.update(dt)
         screen.fill("black")
+        for thing in drawable_group:
+            thing.draw(screen)
+
         pygame.display.flip()
 
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
